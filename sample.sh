efcd5d0c6ad84060f311e4cc64973e5c297a819b
Franz Georg Fuchs <franzgeorgfuchs@gmail.com>
2024-06-26 10:13:17 +0200

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=random_sample
 #              d-hh:mm:ss
 #SBATCH --time=30-00:00:00
-#SBATCH --output=/home/franzf/OpenQuantumComputing_private/OpenQuantumComputing/openquantumcomputing/%j.out
+#SBATCH --output=/somepath/%j.out
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=32
