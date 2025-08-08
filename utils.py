7f3a63a9fa4c42a9b089d12b24235c4c0f97a9bd
dinasl <dinasyverudlindland@gmail.com>
2025-08-08 16:55:03 +0200

    Fixed some logical errors in the code 1) seeds should not be skipped in compute_all_orbits if wanting to find all the largest orbits, 2) is_connected was not strict enough (now requires a chain of overlaps to reach all nodes in the combination). Also did some minor changes in plot_mixers.py.

--- utils.py
+++ utils.py
@@ -89,7 +89,8 @@ def convert_to_binary_string(int_values, n):
 
 def is_connected(orbits):
     """
-    Checks if the provided orbits are connected, meaning that there is at least one node in common between any two orbits.
+    Check if the list of orbits forms a connected structure.
+    Connectivity means you can move from any orbit to any other via overlapping nodes.  
 
     Args:
         orbits (List[Tuple[int, ...]]): Node indices representing the group generated sets.
@@ -97,13 +98,30 @@ def is_connected(orbits):
     Returns:
         bool: True if the orbits are connected, False otherwise.
     """
-    # Short circuit evaluation
-    if len(orbits) == 1: return True # Return True if there is only one orbit (connecting all nodes)
-    for orbit in orbits:
-        if not any(set(orbit).intersection(set(other_orbit)) for other_orbit in orbits if other_orbit != orbit):
-            return False
-    return True
+    if len(orbits) <= 1:
+        return True
 
+    # Convert orbits to sets for quick intersection
+    orbit_sets = [set(o) for o in orbits]
+
+    # Build adjacency list: two orbits connected if their sets overlap
+    adjacency = {i: set() for i in range(len(orbit_sets))}
+    for i in range(len(orbit_sets)):
+        for j in range(i + 1, len(orbit_sets)):
+            if orbit_sets[i].intersection(orbit_sets[j]):
+                adjacency[i].add(j)
+                adjacency[j].add(i)
+
+    visited = set()
+    stack = [0]  # start from first orbit
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.add(node)
+            stack.extend(adjacency[node] - visited)
+
+    return len(visited) == len(orbit_sets)
 
 def find_best_cost(Xs, Zs_operators):
     """
