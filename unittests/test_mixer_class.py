62ea4a3d1397ec4164e2feb3ece03765e1e1e680
sannesg <Sanne.selmer.gilje@sintef.no>
2025-08-08 09:22:01 +0200

    last commit message is wrong, there was a mismatch in how the structure used to be and how it is structured now and gave a wrong OK-unittest. None work per now (and per last commit)

--- unittests/test_mixer_class.py
+++ unittests/test_mixer_class.py
@@ -13,7 +13,7 @@ parent_dir = current_dir.parent
 sys.path.append(str(parent_dir))
 
 from Mixer import LXMixer, Orbit
-
+"""
 class TestLXMixer(unittest.TestCase):
     def test_compute_family_of_graphs(self):
         correct_family_of_valid_graphs = {
@@ -66,14 +66,14 @@ class TestLXMixer(unittest.TestCase):
             orbits[nodes] = orbit.Xs
         
         self.assertEqual(orbits, correct_orbits)
-
+"""
 class TestStabilizer(unittest.TestCase):
     def setUp(self):
         self.test_cases = [
             {
                 "B": [0b1011, 0b1100, 0b0111, 0b0000, 0b1110, 0b1001, 0b0010, 0b0101],
                 "n": 4,
-                "orbit_dictionary": {(0,1,2,3,4,5,6,7): Orbit(Xs={11, 14, 7})},
+                "orbits": {(0,1,2,3,4,5,6,7): Orbit(Xs={11, 14, 7})},
                 "expected_orbits": {(0,1,2,3,4,5,6,7) : [11, 14, 7]},
                 "expected_mgs": {(0,1,2,3,4,5,6,7):[(1, 13)]},
                 "expected_projectors": {(0,1,2,3,4,5,6,7): [(1, 0), (1, 13)]}
@@ -81,20 +81,19 @@ class TestStabilizer(unittest.TestCase):
             {
                 "B": [0b11000, 0b00100, 0b01101, 0b10001],
                 "n": 5,
-                "orbit_dictionary": {(0,1,2,3): Orbit(Xs={9, 28})},
+                "orbits": {(0,1,2,3): Orbit(Xs={9, 28})},
                 "expected_orbits": {(0,1,2,3): [9, 28]},
                 "expected_mgs": {(0,1,2,3) : [(1, 2), (-1, 20), (1, 25)]},
                 "expected_projectors": {(0,1,2,3) : [(1, 0), (1, 25), (-1, 20), (-1, 13), (1, 2), (1, 27), (-1, 22), (-1, 15)]}
+            }, #TODO the one below is not working....
+            {
+                "B": [0b1110, 0b1100, 0b1001, 0b0100, 0b0011],
+                "n": 4,
+                "orbit_dictionary": {(0,2,3,4): Orbit(Xs={10, 13, 7}), (0,1): Orbit(Xs={2}), (1,2): Orbit(Xs={5}), (1,3): Orbit(Xs={8}), (1,4): Orbit(Xs={15})},
+                "expected_orbits": {(0,1,2,3,4,5,6,7) : [11, 14, 7]},
+                "expected_mgs": {(0,1,2,3,4,5,6,7):[(1, 13)]},
+                "expected_projectors": {(0,2,3,4): [(1,0), (-1, 14), (-1, 5), (1, 11)], (0,1): [(1, 0), (1, 1), (-1, 4), (-1, 5), (-1, 8), (-1, 9), (1, 12), (1, 13)], (1,2): [(1, 0), (-1, 5), (1, 2), (-1, 7), (-1, 8), (1, 13), (-1, 10), (1, 15)], (1,3): [(1, 0), (1, 1), (1, 2), (1, 3), (-1, 4), (-1, 5), (-1, 6), (-1, 7)], (1,4): [(1, 0), (-1, 9), (-1, 10), (1, 3), (1, 12), (-1, 5), (-1, 6), (1, 15)]}
             }
-            # }, #TODO the one below is not working....
-            # {
-            #     "B": [0b1110, 0b1100, 0b1001, 0b0100, 0b0011],
-            #     "n": 4,
-            #     "orbit_dictionary": {(0,2,3,4): Orbit(Xs={10, 13, 7}), (0,1): Orbit(Xs={2}), (1,2): Orbit(Xs={5}), (1,3): Orbit(Xs={8}), (1,4): Orbit(Xs={15})},
-            #     "expected_orbits": {(0,1,2,3,4,5,6,7) : [11, 14, 7]},
-            #     "expected_mgs": {(0,1,2,3,4,5,6,7):[(1, 13)]},
-            #     "expected_projectors": {(0,2,3,4): [(1,0), (-1, 14), (-1, 5), (1, 11)], (0,1): [(1, 0), (1, 1), (-1, 4), (-1, 5), (-1, 8), (-1, 9), (1, 12), (1, 13)], (1,2): [(1, 0), (-1, 5), (1, 2), (-1, 7), (-1, 8), (1, 13), (-1, 10), (1, 15)], (1,3): [(1, 0), (1, 1), (1, 2), (1, 3), (-1, 4), (-1, 5), (-1, 6), (-1, 7)], (1,4): [(1, 0), (-1, 9), (-1, 10), (1, 3), (1, 12), (-1, 5), (-1, 6), (1, 15)]}
-            # }
         ]
 
     def test_compute_minimal_generating_sets(self):
@@ -103,6 +102,7 @@ class TestStabilizer(unittest.TestCase):
                 case = copy.deepcopy(case)
                 lx = LXMixer(B=case["B"], nL=case["n"], method="largest_orbits")
                 lx.compute_minimal_generating_sets()
+                print("min_gen_sets: ", [orbit.Zs for orbit in lx.orbits.values()])
                 for orbit_key, orbit in lx.orbits.items():
                     with self.subTest(orbit_key=orbit_key):
                         self.assertEqual(orbit.Zs, case["expected_mgs"][orbit_key])
@@ -120,7 +120,60 @@ class TestStabilizer(unittest.TestCase):
                 for orbit_key, orbit in lx.orbits.items():
                     with self.subTest(orbit_key=orbit_key):
                         self.assertEqual(orbit.Zs, case["expected_projectors"][orbit_key])
+    """
+    def test_compute_family_of_graphs(self):
+        for case in self.test_cases:
+            with self.subTest(case=case):
+                case = copy.deepcopy(case)
+                lx = LXMixer(B=case["B"], nL=case["n"], method="largest_orbits")
+                correct_family_of_valid_graphs = {
+                    0b0010: [(0, 1)],
+                    0b0101: [(1, 2)],
+                    0b0111: [(0, 2), (3, 4)],
+                    0b1000: [(1, 3)],
+                    0b1010: [(0, 3), (2, 4)],
+                    0b1101: [(0, 4), (2, 3)],
+                    0b1111: [(1, 4)]
+                }
+        
+                lx.compute_family_of_valid_graphs()
+        
+                # Sort edges for comparison
+                for key in correct_family_of_valid_graphs:
+                    correct_family_of_valid_graphs[key] = sorted(correct_family_of_valid_graphs[key])
+                
+                for key in lx.family_of_valid_graphs:
+                    lx.family_of_valid_graphs[key] = sorted(lx.family_of_valid_graphs[key])
+                
+                self.assertEqual(lx.family_of_valid_graphs, correct_family_of_valid_graphs)
+    
+    def test_compute_all_orbits(self):
+        correct_orbits = {
+            (0, 2, 3, 4) : [0b1101, 0b1010],
+            (0, 1) : [0b0010],
+            (1, 2) : [0b0101],
+            (1, 3) : [0b1000],
+            (1, 4) : [0b1111]
+        }
+        
 
+        lx.family_of_valid_graphs = {
+            0b0010: [(0, 1)],
+            0b0101: [(1, 2)],
+            0b0111: [(0, 2), (3, 4)],
+            0b1000: [(1, 3)],
+            0b1010: [(0, 3), (2, 4)],
+            0b1101: [(0, 4), (2, 3)],
+            0b1111: [(1, 4)]
+        }
+        lx.compute_all_orbits()
+        
+        orbits = {}
+        for nodes, orbit in lx.orbits.items():
+            orbits[nodes] = orbit.Xs
+        
+        self.assertEqual(orbits, correct_orbits)
+    """
     
 if __name__ == '__main__':
     unittest.main()
